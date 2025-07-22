cdc4bc392e3cd706a3996973ce5a1521231c7b85
Laura Barauna <lauravbarauna@gmail.com>
2025-07-22 19:39:42 -0300

    feat: add admin update and find methods in repository and service

--- src/main/java/br/com/lauraBarauna/repository/admin/AdminRepository.java
+++ src/main/java/br/com/lauraBarauna/repository/admin/AdminRepository.java
@@ -2,10 +2,16 @@ package br.com.lauraBarauna.repository.admin;
 
 import br.com.lauraBarauna.db.MySQLConnection;
 import br.com.lauraBarauna.model.admin.Admin;
+import br.com.lauraBarauna.model.common.Email;
+import br.com.lauraBarauna.model.common.Phone;
+import br.com.lauraBarauna.model.user.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdminRepository {
 
@@ -23,6 +29,81 @@ public class AdminRepository {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public void updateAdmin(Admin admin) {
+        String sql = "UPDATE admins SET additional_role = ? WHERE user_id = ?";
+
+        try (Connection conn = MySQLConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1,admin.getAdditionalRole());
+            stmt.setInt(2,admin.getUser().getId());
+
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Admin findAdminById(int userId) {
+        String sql =
+                "SELECT a.user_id AS admin_id, u.name, u.email, u.phone, a.additional_role" +
+                "FROM admins a" +
+                "JOIN users u ON a.user_id = u.id" +
+                "WHERE a.user_id = ?";
+
+        try (Connection conn = MySQLConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, userId);
+            try (ResultSet rs = stmt.executeQuery()) {
+                if (rs.next()) {
+                    String name = rs.getString("name");
+                    String email = rs.getString("email");
+                    String phone = rs.getString("phone");
+                    Email emailObj = Email.fromDatabase(email);
+                    Phone phoneObj = Phone.fromDataBase(phone);
+                    String role = rs.getString("additional_role");
+                    return Admin.fromDataBase(User.fromDataBase(userId, name, emailObj, null, phoneObj), role);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<Admin> findAllAdmins() {
+        List<Admin> admins = new ArrayList<>();
+        String sql =
+                "SELECT a.user_id AS admin_id, u.name, u.email, u.phone, a.additional_role" +
+                        "FROM admins a" +
+                        "JOIN users u ON a.user_id = u.id";
+
+        try (Connection conn = MySQLConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                int id = rs.getInt("admin_id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String phone = rs.getString("phone");
+                Email emailObj = Email.fromDatabase(email);
+                Phone phoneObj = Phone.fromDataBase(phone);
+                String role = rs.getString("additional_role");
+                User user = User.fromDataBase(id, name, emailObj, null, phoneObj);
+                admins.add(Admin.fromDataBase(user, role));
+            }
+            return admins;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+
     }
 
 }
--- src/main/java/br/com/lauraBarauna/service/admin/AdminService.java
+++ src/main/java/br/com/lauraBarauna/service/admin/AdminService.java
@@ -2,6 +2,7 @@ package br.com.lauraBarauna.service.admin;
 
 import br.com.lauraBarauna.dto.admin.AdminRequestDTO;
 import br.com.lauraBarauna.model.admin.Admin;
+import br.com.lauraBarauna.model.user.User;
 import br.com.lauraBarauna.repository.admin.AdminRepository;
 import br.com.lauraBarauna.service.user.UserService;
 
@@ -12,5 +13,10 @@ public class AdminService {
 
     public void createAdmin(AdminRequestDTO adminDTO) {
         this.REPOSITORY.addAdmin(Admin.fromCreation(this.USER_SERVICE.getUserByEmail(adminDTO.getEmail()), adminDTO.getAdditionalRole()));
+    }
+
+    public void updateAdmin(AdminRequestDTO adminDTO) {
+        User user = this.USER_SERVICE.getUserByEmail(adminDTO.getEmail());
+        this.REPOSITORY.updateAdmin(Admin.fromUpdate(user, adminDTO.getAdditionalRole()));
     }
 }
